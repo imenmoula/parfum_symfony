f25e9fee9ffbb511ef33715279b3184eaa4a4c19
moulaimen <votre_email>
2024-05-14 07:43:13 +0200

    fix list user

---
+++ src/Controller/UserController.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\User;
+class UserController extends AbstractController
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+
+    #[Route('/user', name: 'app_user')]
+    public function index(): Response
+    {
+        $users = $this->entityManager->getRepository(User::class)->findAll();
+
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+            'users' => $users, // Pass the retrieved users to the template
+        ]);
+    }
+ 
+}
